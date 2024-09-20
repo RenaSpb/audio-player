a2ce21ffe51a5c4ad0dbb247fe8170932954991d
ReaSpb <murzinarenata@gmail.com>
2024-09-20 12:47:21 -0700

    feat: add next and previus button

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 const tracks = [
-    { title: "Эмигрант эмигранту", artist: "Каста", src: "audio-player/tracks/01.mp3" },
-    { title: "Кровопровод «Дружба»", artist: "Каста", src: "audio-player/tracks/02.mp3" },
-    { title: "Священный мясокомбинат", artist: "Каста", src: "audio-player/tracks/03.mp3" },
-    { title: "Ссылочка ВКонтакте", artist: "Каста", src: "audio-player/tracks/04.mp3" },
-    { title: "ОбманУтый", artist: "Каста", src: "audio-player/tracks/05.mp3" },
-    { title: "Космос и Говно", artist: "Каста", src: "audio-player/tracks/06.mp3" },
-    { title: "Созвон", artist: "Каста", src: "audio-player/tracks/07.mp3" },
-    { title: "Пионерская правда", artist: "Каста", src: "audio-player/tracks/08.mp3" },
-    { title: "Степень ватности", artist: "Каста", src: "audio-player/tracks/09.mp3" },
-    { title: "Зима снится", artist: "Каста", src: "audio-player/tracks/10.mp3" },
-    { title: "Владимирская Русь", artist: "Каста", src: "audio-player/tracks/11.mp3" },
-    { title: "Багаж", artist: "Каста", src: "audio-player/tracks/12.mp3" },
-    { title: "Этого больше не будет", artist: "Каста", src: "audio-player/tracks/13.mp3" },
-    { title: "По-разному", artist: "Каста", src: "audio-player/tracks/14.mp3" },
-    { title: "Сказка «Чёрная краска»", artist: "Каста", src: "audio-player/tracks/15.mp3" }
+    { title: "Эмигрант эмигранту", artist: "Каста", src: "audio-player/tracks/01.mp3", photo: 'audio-player/photos/1.jpg' },
+    { title: "Кровопровод «Дружба»", artist: "Каста", src: "audio-player/tracks/02.mp3", photo: 'audio-player/photos/2.jpg' },
+    { title: "Священный мясокомбинат", artist: "Каста", src: "audio-player/tracks/03.mp3", photo: 'audio-player/photos/3.jpg' },
+    { title: "Ссылочка ВКонтакте", artist: "Каста", src: "audio-player/tracks/04.mp3", photo: 'audio-player/photos/4.jpg' },
+    { title: "ОбманУтый", artist: "Каста", src: "audio-player/tracks/05.mp3", photo: 'audio-player/photos/5.jpg' },
+    { title: "Космос и Говно", artist: "Каста", src: "audio-player/tracks/06.mp3", photo: 'audio-player/photos/6.jpg' },
+    { title: "Созвон", artist: "Каста", src: "audio-player/tracks/07.mp3", photo: 'audio-player/photos/7.jpg' },
+    { title: "Пионерская правда", artist: "Каста", src: "audio-player/tracks/08.mp3", photo: 'audio-player/photos/8.jpg' },
+    { title: "Степень ватности", artist: "Каста", src: "audio-player/tracks/09.mp3", photo: 'audio-player/photos/9.jpg' },
+    { title: "Зима снится", artist: "Каста", src: "audio-player/tracks/10.mp3", photo: 'audio-player/photos/10.jpg' },
+    { title: "Владимирская Русь", artist: "Каста", src: "audio-player/tracks/11.mp3", photo: 'audio-player/photos/11.jpg'},
+    { title: "Багаж", artist: "Каста", src: "audio-player/tracks/12.mp3", photo: 'audio-player/photos/12.jpg' },
+    { title: "Этого больше не будет", artist: "Каста", src: "audio-player/tracks/13.mp3", photo: 'audio-player/photos/13.jpg' },
+    { title: "По-разному", artist: "Каста", src: "audio-player/tracks/14.mp3", photo: 'audio-player/photos/14.jpg' },
+    { title: "Сказка «Чёрная краска»", artist: "Каста", src: "audio-player/tracks/15.mp3", photo: 'audio-player/photos/15.jpg' }
 ];
 
-const playButton = document.getElementById('play')
-const audioPlayer = document.getElementById('audio-player')
+const playButton = document.getElementById('play');
+const audioPlayer = document.getElementById('audio-player');
+const trackTitle = document.querySelector('.track-title');
+const trackArtist = document.querySelector('.track-artist');
+const trackPhoto = document.querySelector('.player-image img');
+const nextButton = document.getElementById('skip-next');
+const previousButton = document.getElementById('skip-previous');
 
 let currentTrackIndex = 0
 
 function startTrack(index) {
-    const track = tracks[index];
+    currentTrackIndex = (index + tracks.length) % tracks.length;
+    const track = tracks[currentTrackIndex];
     audioPlayer.src = track.src;
     audioPlayer.load();
     audioPlayer.play();
-    updatePlayButton();
+    trackTitle.textContent = track.title;
+    trackArtist.textContent = track.artist;
+    trackPhoto.src = track.photo;
+    updatePlayButton()
 }
 
 function updatePlayButton() {
@@ -37,16 +46,30 @@ function togglePlayPause() {
     if (audioPlayer.paused) {
         audioPlayer.play();
     } else {
-        audioPlayer.pause()
+        audioPlayer.pause();
     }
-    updatePlayButton();
+    updatePlayButton()
 }
 
-playButton.addEventListener('click', ()=> {
+function playNextTrack () {
+    startTrack(currentTrackIndex + 1)
+}
+
+function playPreviousTrack() {
+    startTrack(currentTrackIndex - 1)
+}
+
+nextButton.addEventListener('click', playNextTrack);
+previousButton.addEventListener('click', playPreviousTrack);
+
+playButton.addEventListener('click', () => {
     if (audioPlayer.src) {
         togglePlayPause();
-    } else startTrack(currentTrackIndex)
+    } else {
+        startTrack(currentTrackIndex)
+    }
 })
 
-playButton.addEventListener('play', updatePlayButton)
-playButton.addEventListener('pause', updatePlayButton)
+audioPlayer.addEventListener('play', updatePlayButton)
+audioPlayer.addEventListener('pause', updatePlayButton)
+audioPlayer.addEventListener('ended', playNextTrack);
